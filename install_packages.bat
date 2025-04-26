1b94b02c2eed8f94cb1835988150d192fca20b84
HanabiXJP <134588660+HanabiXJP@users.noreply.github.com>
2025-04-26 12:39:26 +0700

    Add install_packages.bat

---
+++ install_packages.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing required Python packages...
+python -m pip install pandas numpy fuzzywuzzy matplotlib seaborn scikit-learn optuna joblib
+echo Installation complete.
+pause
